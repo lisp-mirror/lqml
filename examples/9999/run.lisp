7b09ca64b779353076c63dc1f9d6726ab510b6c0
pls.153 <pls.153@protonmail.com>
2022-02-13 12:23:54 +0100

    revision

--- examples/9999/run.lisp
+++ examples/9999/run.lisp
@@ -15,5 +15,5 @@
 ;;; to your Slime directory, which is assumed to be '~/slime/'
 
 (when (find "-slime" (ext:command-args) :test 'string=)
-  (load "~/slime/qml-start-swank")) ; for 'slime-connect' from Emacs
+  (load "~/slime/lqml-start-swank")) ; for 'slime-connect' from Emacs
 
