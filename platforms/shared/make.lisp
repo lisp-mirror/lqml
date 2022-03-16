fee435876027be16b24f31bb79c45a7379a37074
pls.153 <pls.153@protonmail.com>
2022-03-16 14:34:37 +0100

    simplify shared 'make.lisp'

--- platforms/shared/make.lisp
+++ platforms/shared/make.lisp
@@ -1,5 +1,5 @@
 ;;; cross-compile ASDF system, using the byte-codes compiler as an intermediate
-;;; step; this requires an ASDF hack, but won't even try to build useless
+;;; step; this requires a small ASDF hack, but won't even try to build useless
 ;;; cross-compiled *.fas files -- we load byte-compiled *.fasc files instead
 ;;;
 ;;; this is both faster and avoids a 'ld' error when cross-compiling for iOS
@@ -65,8 +65,6 @@
 
 (load (merge-pathnames "src/lisp/tr.lisp")) ; i18n
 
-;;; --- ASDF hack begin ---
-
 (in-package :asdf/lisp-action)
 
 (defmethod asdf:perform ((o asdf:load-op) (c asdf:cl-source-file))
@@ -76,48 +74,11 @@
       (setf fasl (cl-user::cc (namestring fasl) "c"))
       (load fasl))))
 
-(in-package :uiop/lisp-build)
-
-(defun* (compile-file*) (input-file &rest keys
-                                    &key (compile-check *compile-check*) output-file warnings-file
-                                    object-file
-                                    &allow-other-keys)
-    (when (and object-file (equal (compile-file-type) (pathname object-file)))
-      (format t "Whoa, some funky ASDF upgrade switched ~S calling convention for ~S and ~S~%"
-              'compile-file* output-file object-file)
-      (rotatef output-file object-file))
-    (let* ((keywords (remove-plist-keys
-                      `(:output-file :compile-check :warnings-file :object-file) keys))
-           (output-file
-             (or output-file
-                 (apply 'compile-file-pathname* input-file :output-file output-file keywords)))
-           (physical-output-file (physicalize-pathname output-file))
-           (object-file
-             (unless (use-ecl-byte-compiler-p)
-               (or object-file
-                   (compile-file-pathname output-file :type :object)))))
-      (multiple-value-bind (output-truename warnings-p failure-p)
-          (with-enough-pathname (input-file :defaults *base-build-directory*)
-            (with-saved-deferred-warnings (warnings-file :source-namestring (namestring input-file))
-              (with-muffled-compiler-conditions ()
-                ;; cross-compile *.o file without even trying to build useless
-                ;; cross-compiled *.fas file, which would give a 'ld' error
-                ;; when cross-compiling for iOS (wrong flags)
-                (when (or (not (probe-file object-file))
-                          (> (file-write-date input-file)
-                             (file-write-date object-file)))
-                  (apply 'compile-file input-file
-                         :output-file (list* object-file :system-p t keywords)))
-                  (let ((name (namestring object-file)))
-                    (setf name (cl-user::cc (subseq name 0 (1- (length name))) "fas"))
-                    ;; create dummy to make ASDF happy
-                    (open name :direction :probe :if-does-not-exist :create)
-                    (pathname name)))))
-        (values output-truename warnings-p failure-p))))
-
 (in-package :cl-user)
 
-;;; --- ASDF hack end ---
+(defun c:build-fasl (file &rest _)
+  ;; do nothing (not needed when cross-compiling)
+  file)
 
 (asdf:make-build *asdf-system*
                  :monolithic t
