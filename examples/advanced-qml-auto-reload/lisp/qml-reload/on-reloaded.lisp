ae4d2020e3cc7999bbe92c55085e5dcb58ff1d29
pls.153 <pls.153@protonmail.com>
2022-03-18 08:20:19 +0100

    review comment

--- examples/advanced-qml-auto-reload/lisp/qml-reload/on-reloaded.lisp
+++ examples/advanced-qml-auto-reload/lisp/qml-reload/on-reloaded.lisp
@@ -1,6 +1,8 @@
-;;; this file will be loaded every time QML has been reloaded
+;;; this file will be loaded every time 'main.qml' has been reloaded
 
 (in-package :qml-user)
 
-;; delay needed here because of initial reload
+;;; delay needed here because of indirect QML Loader, needed for
+;;; single file auto reload
+
 (qsingle-shot 1000 (lambda () (eval:eval-in-thread "(qml::help)"))) ; show help in REPL
