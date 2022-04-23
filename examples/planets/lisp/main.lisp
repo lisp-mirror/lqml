59caa6372deccf371007615f7e4667e535f05b27
pls.153 <pls.153@protonmail.com>
2022-04-23 07:50:19 +0200

    revision of example 'planets'

--- examples/planets/lisp/main.lisp
+++ examples/planets/lisp/main.lisp
@@ -19,59 +19,64 @@
 ;;;
 ;;;   (q! |remove| *planets* 0)
 ;;;   (q! |clear| *planets*)
-;;;   (q! |setProperty| 0 "name" "First")
+;;;   (q! |setProperty| *planets* 0 "name" "First")
+;;;
+;;; The model keys (like ':m-name') are all preceeded by 'm-' to prevent
+;;; eventual name clashes with property names in QML. The 'm' stands for
+;;; 'model'. This would not be a problem in this example, but it's a good
+;;; generic rule.
 
 (defun populate-item-model ()
   (q! |clear| *planets*)
   (qjs |addPlanet| *planets*
-       (list :name  "Sun"
-             :shape "img/sun.png"
-             :map   "img/sun-map.jpg"
-             :info  #.(read-file "txt/sun.txt")))
+       (list :m-name  "Sun"
+             :m-shape "img/sun.png"
+             :m-map   "img/sun-map.jpg"
+             :m-info  #.(read-file "txt/sun.txt")))
   (qjs |addPlanet| *planets*
-       (list :name  "Mercury"
-             :shape "img/mercury.png"
-             :map   "img/mercury-map.jpg"
-             :info  #.(read-file "txt/mercury.txt")))
+       (list :m-name  "Mercury"
+             :m-shape "img/mercury.png"
+             :m-map   "img/mercury-map.jpg"
+             :m-info  #.(read-file "txt/mercury.txt")))
   (qjs |addPlanet| *planets*
-       (list :name  "Venus"
-             :shape "img/venus.png"
-             :map   "img/venus-map.jpg"
-             :info  #.(read-file "txt/venus.txt")))
+       (list :m-name  "Venus"
+             :m-shape "img/venus.png"
+             :m-map   "img/venus-map.jpg"
+             :m-info  #.(read-file "txt/venus.txt")))
   (qjs |addPlanet| *planets*
-       (list :name  "Earth"
-             :shape "img/earth.png"
-             :map   "img/earth-map.jpg"
-             :info  #.(read-file "txt/earth.txt")))
+       (list :m-name  "Earth"
+             :m-shape "img/earth.png"
+             :m-map   "img/earth-map.jpg"
+             :m-info  #.(read-file "txt/earth.txt")))
   (qjs |addPlanet| *planets*
-       (list :name  "Moon"
-             :shape "img/moon.png"
-             :map   "img/moon-map.jpg"
-             :info  #.(read-file "txt/moon.txt")))
+       (list :m-name  "Moon"
+             :m-shape "img/moon.png"
+             :m-map   "img/moon-map.jpg"
+             :m-info  #.(read-file "txt/moon.txt")))
   (qjs |addPlanet| *planets*
-       (list :name "Mars"
-             :shape "img/mars.png"
-             :map   "img/mars-map.jpg"
-             :info #.(read-file "txt/mars.txt")))
+       (list :m-name "Mars"
+             :m-shape "img/mars.png"
+             :m-map   "img/mars-map.jpg"
+             :m-info #.(read-file "txt/mars.txt")))
   (qjs |addPlanet| *planets*
-       (list :name  "Jupiter"
-             :shape "img/jupiter.png"
-             :map   "img/jupiter-map.jpg"
-             :info  #.(read-file "txt/jupiter.txt")))
+       (list :m-name  "Jupiter"
+             :m-shape "img/jupiter.png"
+             :m-map   "img/jupiter-map.jpg"
+             :m-info  #.(read-file "txt/jupiter.txt")))
   (qjs |addPlanet| *planets*
-       (list :name  "Saturn"
-             :shape "img/saturn.png"
-             :map   "img/saturn-map.jpg"
-             :info  #.(read-file "txt/saturn.txt")))
+       (list :m-name  "Saturn"
+             :m-shape "img/saturn.png"
+             :m-map   "img/saturn-map.jpg"
+             :m-info  #.(read-file "txt/saturn.txt")))
   (qjs |addPlanet| *planets*
-       (list :name  "Uranus"
-             :shape "img/uranus.png"
-             :map   "img/uranus-map.jpg"
-             :info  #.(read-file "txt/uranus.txt")))
+       (list :m-name  "Uranus"
+             :m-shape "img/uranus.png"
+             :m-map   "img/uranus-map.jpg"
+             :m-info  #.(read-file "txt/uranus.txt")))
   (qjs |addPlanet| *planets*
-       (list :name  "Neptune"
-             :shape "img/neptune.png"
-             :map   "img/neptune-map.jpg"
-             :info  #.(read-file "txt/neptune.txt"))))
+       (list :m-name  "Neptune"
+             :m-shape "img/neptune.png"
+             :m-map   "img/neptune-map.jpg"
+             :m-info  #.(read-file "txt/neptune.txt"))))
 
 (populate-item-model)
--- examples/planets/qml/main.qml
+++ examples/planets/qml/main.qml
@@ -24,7 +24,7 @@ Item {
     objectName: "planets"
 
     // example of inline item
-    //ListElement { name: "Earth"; shape: "img/earth.png"; map: "img/earth-map.jpg"; info: "..." }
+    //ListElement { mName: "Earth"; mShape: "img/earth.png"; mMap: "img/earth-map.jpg"; mInfo: "..." }
 
     function addPlanet(planet) { append(planet) }
   }
@@ -53,7 +53,7 @@ Item {
           anchors.leftMargin: 4
           font.pixelSize: parent.height - 22
           color: "#f0f0f0"
-          text: name // see Lisp keyword name
+          text: mName // see Lisp keyword name
         }
       }
 
@@ -76,7 +76,7 @@ Item {
             height: parent.height - mapImage.height
             width: parent.width
             fillMode: Image.PreserveAspectFit
-            source: shape // see Lisp keyword name
+            source: mShape // see Lisp keyword name
           }
 
           Image {
@@ -84,7 +84,7 @@ Item {
             width: parent.width
             height: 0
             fillMode: Image.PreserveAspectFit
-            source: map // see Lisp keyword name
+            source: mMap // see Lisp keyword name
           }
         }
       }
@@ -136,7 +136,7 @@ Item {
               focus: true
               wrapMode: TextEdit.Wrap
               onCursorRectangleChanged: flick.ensureVisible(cursorRectangle)
-              text: info // see Lisp keyword name
+              text: mInfo // see Lisp keyword name
             }
           }
         }
