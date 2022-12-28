c5b350a9b518f50b688f16bbc4d03b988f13984e
pls.153 <pls.153@protonmail.com>
2022-12-28 20:15:11 +0100

    iOS: prevent auto full stop on double space with DISABLE_SMART_QUOTES

--- src/cpp/main.h
+++ src/cpp/main.h
@@ -28,67 +28,81 @@ public:
 #if (defined Q_OS_IOS) && (defined DISABLE_SMART_QUOTES)
   bool eventFilter(QObject* object, QEvent* event) override {
     if (event->type() == QEvent::InputMethod) {
+      static bool skipAutoFullStop = false;
       QInputMethodEvent* input = static_cast<QInputMethodEvent*>(event);
       QString s = input->commitString();
-      if (s.size() == 1) {
-        bool changed = true;
-        const int code = s.at(0).unicode();
-        switch (code) {
-          // capture Tab (iOS external keyboard), since QML Keys doesn't capture it
-          case '\t':
-            s = QString();
-            keyPressed("Tab", object->objectName());
-            break;
-          // undo automatic double hyphen substitution
-          case 8212:
-            s = "--";
-            break;
-          // replace iOS smart quotation marks with standard ones
-          // (English, French, German, ...)
-          case 8216:
-          case 8217:
-          case 8218:
-            s[0] = QChar('\'');
-            break;
-          case 171:
-          case 187:
-          case 8220:
-          case 8221:
-          case 8222:
-            s[0] = QChar('"');
-            break;
-          // capture Alt+E, Alt+L (iOS external keyboard) for example 'cl-repl'
-          case 8364:
-            s = QString();
-            keyPressed("Alt+E", object->objectName());
-            break;
-          case 172:
-            s = QString();
-            keyPressed("Alt+L", object->objectName());
-            break;
-          default:
-            changed = false;
+      int size = s.size();
+      if (size == 0) {
+        skipAutoFullStop = true;
+      } else {
+        if (size == 1) {
+          bool changed = true;
+          const int code = s.at(0).unicode();
+          switch (code) {
+            case '.':
+              if (skipAutoFullStop) { // prevent auto full stop after double space
+                s = " ";
+              } else {
+                changed = false;
+              }
+              break;
+            // capture Tab (iOS external keyboard), since QML Keys doesn't capture it
+            case '\t':
+              s = QString();
+              keyPressed("Tab", object->objectName());
+              break;
+            // undo automatic double hyphen substitution
+            case 8212:
+              s = "--";
+              break;
+            // replace iOS smart quotation marks with standard ones
+            // (English, French, German, ...)
+            case 8216:
+            case 8217:
+            case 8218:
+              s[0] = QChar('\'');
+              break;
+            case 171:
+            case 187:
+            case 8220:
+            case 8221:
+            case 8222:
+              s[0] = QChar('"');
+              break;
+            // capture Alt+E, Alt+L (iOS external keyboard) for example 'cl-repl'
+            case 8364:
+              s = QString();
+              keyPressed("Alt+E", object->objectName());
+              break;
+            case 172:
+              s = QString();
+              keyPressed("Alt+L", object->objectName());
+              break;
+            default:
+              changed = false;
+          }
+          if (changed) {
+            input->setCommitString(s);
+          }
+        } else if (size == 2) {
+          bool changed = true;
+          s = s.trimmed();
+          const int code = s.at(0).unicode();
+          switch (code) {
+            // replace iOS smart quotation marks with standard ones
+            // (Frensh, ...)
+            case 171:
+            case 187:
+              s[0] = QChar('"');
+              break;
+            default:
+              changed = false;
+          }
+          if (changed) {
+            input->setCommitString(s);
+          }
         }
-        if (changed) {
-          input->setCommitString(s);
-        }
-      } else if (s.size() == 2) {
-        bool changed = true;
-        s = s.trimmed();
-        const int code = s.at(0).unicode();
-        switch (code) {
-          // replace iOS smart quotation marks with standard ones
-          // (Frensh, ...)
-          case 171:
-          case 187:
-            s[0] = QChar('"');
-            break;
-          default:
-            changed = false;
-        }
-        if (changed) {
-          input->setCommitString(s);
-        }
+        skipAutoFullStop = false;
       }
     }
     return QGuiApplication::eventFilter(object, event);
