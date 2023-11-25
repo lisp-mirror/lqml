bd3181e544bf2ebedddef0f0d4df576bc08a4bfa
pls.153 <pls.153@protonmail.com>
2023-11-25 13:08:37 +0100

    revision

--- examples/cl-repl/build-android/install-run.sh
+++ examples/cl-repl/build-android/install-run.sh
@@ -1,3 +1,3 @@
 # install/update (keeps app data)
 adb install -r android-build/*.apk
-adb shell am start -n org.qtproject.example.repl/org.qtproject.qt5.android.bindings.QtActivity # Qt5
+adb shell am start -n org.eql5.android.repl/org.qtproject.qt5.android.bindings.QtActivity # Qt5
